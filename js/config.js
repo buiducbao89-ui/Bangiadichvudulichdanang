294e38a367896e99bb205e532bb33deae99c4c38
buiducbao89-ui <buiducbao89@gmail.com>
2026-06-24 05:50:19 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://ohwryqrtdsflvfavwzcm.supabase.co/rest/v1/',
+  url: 'https://ohwryqrtdsflvfavwzcm.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9od3J5cXJ0ZHNmbHZmYXZ3emNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDQ2MDcsImV4cCI6MjA5Nzc4MDYwN30.GxjhoBULJyCy8-MJZ6M6YANv8QmVdrMtRbsZaY6pXJI',
   adminEmail: 'buiducbao89@gmail.com',
   bucket: 'price-videos'
