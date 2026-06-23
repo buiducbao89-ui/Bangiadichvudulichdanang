bf7ed2a2a25945cbdce00472b566812b719b4964
buiducbao89-ui <buiducbao89@gmail.com>
2026-06-24 04:38:16 +0700

    Update config.js

--- Js/config.js
+++ Js/config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
-  url: '',
-  anonKey: '',
-  adminEmail: 'your-admin-email@example.com',
+  url: 'https://ohwryqrtdsflvfavwzcm.supabase.co/rest/v1/',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9od3J5cXJ0ZHNmbHZmYXZ3emNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDQ2MDcsImV4cCI6MjA5Nzc4MDYwN30.GxjhoBULJyCy8-MJZ6M6YANv8QmVdrMtRbsZaY6pXJI',
+  adminEmail: 'buiducbao89@gmail.com',
   bucket: 'price-videos'
 };
